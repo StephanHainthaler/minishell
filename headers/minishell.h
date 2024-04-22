777da562315ddce37a64982d665d7997db92a117
Jayu <jayuze@gmail.com>
2024-04-22 15:44:14 +0200

    added some bullshit

--- headers/minishell.h
+++ headers/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: shaintha <shaintha@student.42.fr>          +#+  +:+       +#+        */
+/*   By: juitz <juitz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 09:06:26 by shaintha          #+#    #+#             */
-/*   Updated: 2024/04/22 10:04:30 by shaintha         ###   ########.fr       */
+/*   Updated: 2024/04/22 14:51:56 by juitz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,5 +32,7 @@
 # include <termios.h>
 # include <curses.h>
 # include <term.h>
+# include <readline/readline.h>
+# include <readline/history.h>
 
 #endif
--- sources/main.c
+++ sources/main.c
@@ -3,17 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: shaintha <shaintha@student.42.fr>          +#+  +:+       +#+        */
+/*   By: juitz <juitz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 09:06:32 by shaintha          #+#    #+#             */
-/*   Updated: 2024/04/22 09:42:45 by shaintha         ###   ########.fr       */
+/*   Updated: 2024/04/22 15:10:44 by juitz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../headers/minishell.h"
 
+
 int main(int argc, char *argv[])
 {
 	printf("Start of minishell-test!: %d %s\n", argc, argv[0]);
+	char *input = readline(argv[1]);
+	printf("%s", input);
 	return (0);
 }
