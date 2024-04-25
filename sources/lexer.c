8229cef20e034cdb4bb98c1dc6843b5c25156303
Stephan Hainthaler <shaintha@c2r4p4.42vienna.com>
2024-04-25 15:40:51 +0200

    Weekend time :3

--- sources/lexer.c
+++ sources/lexer.c
@@ -6,7 +6,7 @@
 /*   By: shaintha <shaintha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 09:31:04 by shaintha          #+#    #+#             */
-/*   Updated: 2024/04/25 14:05:14 by shaintha         ###   ########.fr       */
+/*   Updated: 2024/04/25 14:43:46 by shaintha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,14 +36,12 @@ int	lex_input(t_lexer *lex)
 	{
 		while (ft_isspace(lex->input[lex->i]) == true)
 			lex->i++;
-		//if (lex->input[lex->i] == '|')			//PIPE_TOKEN		
-			//get_pipe_token(lex);	//new_token = ft_lstnew("PIPE", 6);
+		if (lex->input[lex->i] == '|')		
+			new_token = ft_lstnew(PIPE, "|");
 		if (lex->input[lex->i] == '<' || lex->input[lex->i] == '>')		
-			new_token = get_redir_token(lex);	//RE_DIR_TOKENS
-		else
-			lex->i++;
-		// 	new_token = get_word_token(lex);	//WORD_TOKEN
-
+			new_token = get_redir_token(lex);
+		else 
+			new_token = get_word_token(lex);
 		if (new_token == NULL)
 		{
 			if (lex->token_list != NULL)
@@ -55,33 +53,60 @@ int	lex_input(t_lexer *lex)
 	return (0);
 }
 
-// t_list	*get_word_token(t_lexer *lex)
-// {
-// 	t_list	*new_token;
-// 	char	*attr;
-// 	int		j;
+t_list	*get_word_token(t_lexer *lex)
+{
+	t_list	*new_token;
+	char	*attr;
+	int		j;
 
-// 	attr = NULL;
+	attr = NULL;
+	j = 0;
+	while (lex->input[lex->i] != '\0')
+	{
+		if (ft_isspace(lex->input[lex->i]) == true)
+			break ;
+		lex->i++;
+		j++;
+	}
+	attr = (char *)malloc((j + 1) * sizeof(char));
+	if (attr == NULL)
+		return (NULL);
+	ft_strlcpy(attr, lex->input + (lex->i - j), j + 1);
+	new_token = ft_lstnew(WORD, attr);
+	return (new_token);
+}
+
+t_type	get_redir_type(t_lexer *lex)
+{
+	t_type	type;
 	
-// }
+	if (lex->input[lex->i] == '<' && lex->input[lex->i + 1] != '<')
+		type = RE_IN;
+	if (lex->input[lex->i] == '>' && lex->input[lex->i + 1] != '>')
+		type = RE_OUT;
+	lex->i++;
+	if (lex->input[lex->i] == '<' && lex->input[lex->i - 1] == '<')
+	{
+		type = HERE_DOC;
+		lex->i++;
+	}
+	if (lex->input[lex->i] == '>' && lex->input[lex->i - 1] == '>')
+	{
+		type = APPEND;
+		lex->i++;
+	}
+	return (type);
+}
 
 t_list	*get_redir_token(t_lexer *lex)
 {
 	t_list	*new_token;
-	char	*attr;
 	t_type	type;
+	char	*attr;
 	int		j;
 
 	attr = NULL;
-	if (lex->input[lex->i] == '<')
-		type = RE_IN;
-	if (lex->input[lex->i] == '>')
-		type = RE_OUT;
-	lex->i++;
-	if (lex->input[lex->i] == '<' && lex->input[lex->i - 1] == '<')
-		type = HERE_DOC;
-	if (lex->input[lex->i] == '>' && lex->input[lex->i - 1] == '>')
-		type = APPEND;
+	type = get_redir_type(lex);
 	while (ft_isspace(lex->input[lex->i]) == true)
 		lex->i++;
 	if (lex->input[lex->i] == '\0')
@@ -97,9 +122,7 @@ t_list	*get_redir_token(t_lexer *lex)
 	attr = (char *)malloc((j + 1) * sizeof(char));
 	if (attr == NULL)
 		return (NULL);
-	ft_strlcpy(attr, lex->input + (lex->i - j), j);
-	printf("%s\n", attr);
-	printf("%c\n", lex->input[lex->i]);
-	new_token = ft_lstnew(attr, type);
+	ft_strlcpy(attr, lex->input + (lex->i - j), j + 1);
+	new_token = ft_lstnew(type, attr);
 	return (new_token);
 }
