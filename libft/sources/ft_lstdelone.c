c64e4c13b1c96c4157adcddb4faad8340a0f471b
Stephan Hainthaler <shaintha@c2r4p4.42vienna.com>
2024-04-29 12:18:37 +0200

    Push to pull.

--- libft/sources/ft_lstdelone.c
+++ libft/sources/ft_lstdelone.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstdelone.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: shaintha <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: shaintha <shaintha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 10:34:53 by shaintha          #+#    #+#             */
-/*   Updated: 2023/11/13 10:34:54 by shaintha         ###   ########.fr       */
+/*   Updated: 2024/04/29 12:02:02 by shaintha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ void	ft_lstdelone(t_list **lst, t_list *node)
 	if (node != NULL)
 	{
 		to_delete = node;
+		if (to_delete->attr != NULL && to_delete->attr[0] != '|')
+			free(to_delete->attr);
 		node = node->next;
 		free(to_delete);
 		*lst = node;
