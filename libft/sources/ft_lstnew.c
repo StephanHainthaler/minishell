4b68c5a989fe5fca5852b7fa0c8242bfa8064b35
Stephan Hainthaler <shaintha@c2r4p4.42vienna.com>
2024-05-03 15:20:07 +0200

    Updated ft_lstnew.c

--- libft/sources/ft_lstnew.c
+++ libft/sources/ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: shaintha <shaintha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 10:33:31 by shaintha          #+#    #+#             */
-/*   Updated: 2024/04/25 14:03:56 by shaintha         ###   ########.fr       */
+/*   Updated: 2024/05/03 12:05:18 by shaintha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,10 @@ t_list	*ft_lstnew(t_type type, char *attr)
 		return (NULL);
 	new_lst->type = type;
 	new_lst->attr = attr;
+	new_lst->in_squotes = false;
+	new_lst->in_dquotes = false;
+	new_lst->quote_start = -1;
+	new_lst->quote_end = -1;
 	new_lst->next = NULL;
 	return (new_lst);
 }
