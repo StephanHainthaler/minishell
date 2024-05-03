c0306aae67bbb5def61643a34ffb982bdb8640b9
Jayu <jayuze@gmail.com>
2024-05-03 15:04:51 +0200

    added input checks for RE_OUT, HERE_DOC, APPEND, but not working yet

--- sources/parser.c
+++ sources/parser.c
@@ -6,7 +6,7 @@
 /*   By: juitz <juitz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/02 13:26:12 by juitz             #+#    #+#             */
-/*   Updated: 2024/05/03 12:54:25 by juitz            ###   ########.fr       */
+/*   Updated: 2024/05/03 15:02:35 by juitz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,12 @@ int check_valid_input(t_lexer *lex)
 	{
 		if (current->type == RE_IN && current->next != NULL && current->next->type != WORD)
 			return (ft_error("parse error near `<'"), 1);
+		if (current->type == RE_OUT && current->next != NULL && current->next->type != WORD)
+			return (ft_error("parse error near `>'"), 1);
+		if (current->type == HERE_DOC && current->next != NULL && current->next->type != WORD)
+			return (ft_error("parse error near `<<'"), 1);
+		if (current->type == APPEND && current->next != NULL && current->next->type != WORD)
+			return (ft_error("parse error near `>>'"), 1);
 		current = current->next;
 	}
 	lex->token_list = head;
