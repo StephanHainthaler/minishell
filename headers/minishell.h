b14525cd6fd2a6cf9f8e1f89ad504eed3fbfc505
Jayu <jayuze@gmail.com>
2024-05-03 16:38:16 +0200

    updated input token checks

--- headers/minishell.h
+++ headers/minishell.h
@@ -6,7 +6,7 @@
 /*   By: juitz <juitz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 09:06:26 by shaintha          #+#    #+#             */
-/*   Updated: 2024/05/03 12:12:32 by juitz            ###   ########.fr       */
+/*   Updated: 2024/05/03 15:54:45 by juitz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,8 +33,6 @@
 # include <termios.h>
 # include <curses.h>
 # include <term.h>
-# include <readline/history.h>
-
 
 typedef struct s_simp_cmd
 {
@@ -46,31 +44,44 @@ typedef struct s_simp_cmd
 typedef struct s_cmd
 {
 	t_simp_cmd	**simp_cmds;
-	int		num_of_aval_simp_cmds;
-	int		num_of_simp_cmds;
-	char	*infile;
-	char	*outfile;
-	char	*errfile;
-	int		background;
-}			t_cmd;
+	int			num_of_aval_simp_cmds;
+	int			num_of_simp_cmds;
+	char		*infile;
+	char		*outfile;
+	char		*errfile;
+	int			background;
+}				t_cmd;
 
 typedef struct s_lexer
 {
 	t_list	*token_list;
 	char	*input;
 	int		i;
-}			t_lexer;	
+	char	**envp;
+	//bool	is_in_single_quotes;
+	//bool	is_in_double_quotes;
+}			t_lexer;
 
+typedef struct s_minishell
+{
+	int		argc;
+	char	**argv;
+	char	**envp;
+}			t_minishell;
 
-
-int	read_input(void);
-int	lex_input(t_lexer *lex);
-int	check_valid_input(t_lexer *lex);
-t_type	get_pipe_type(t_lexer *lex);
+//lexer.c
+int	read_input(t_minishell *ms);
+int		lex_input(t_lexer *lex);
+int		check_valid_input(t_lexer *lex);
 t_list	*get_word_token(t_lexer *lex);
-t_list	*get_redir_token(t_lexer *lex);
-
-
-
+t_list	*get_non_word_token(t_lexer *lex);
+bool	is_token(char c);
+int	handle_quotes(t_lexer *lex, char quote, int *len);
+//t_list	*handle_quotes(t_lexer *lex);
+int		check_for_expansion(t_list **token_list, char **envp);
+char	*handle_expansion(char *to_expand, char **envp, int *i);
+char	*handle_invalid_expansion(char *str, int len);
+void	expand_env(t_lexer *lex, t_list **lst);
+char	*handle_valid_expansion(char *to_expand, char *env, int len);
 
 #endif
