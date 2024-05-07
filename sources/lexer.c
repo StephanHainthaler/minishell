583e7170f51720bd3edd1d7932fec436c53ab652
Stephan Hainthaler <shaintha@c2r4p4.42vienna.com>
2024-05-07 11:39:54 +0200

    Added new dequotation for token_list.

--- sources/lexer.c
+++ sources/lexer.c
@@ -6,7 +6,7 @@
 /*   By: shaintha <shaintha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 09:31:04 by shaintha          #+#    #+#             */
-/*   Updated: 2024/05/06 15:48:03 by shaintha         ###   ########.fr       */
+/*   Updated: 2024/05/07 11:37:28 by shaintha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,11 +29,16 @@ int	read_input(t_minishell *ms)
 		return (1);
 	printf("Before expansion: \n");
 	ft_putlst_fd(lex.token_list, 1);
-	check_for_expansion(&lex.token_list, ms->envp);
+	if (check_for_expansion(&lex.token_list, ms->envp) == 1)
+		return (1);
 	printf("After expansion: \n");
 	ft_putlst_fd(lex.token_list, 1);
-	ft_lstclear(&lex.token_list);
-	return (0);
+	printf("Before dequotation: \n");
+	if (check_for_dequotation(&lex.token_list) == 1)
+		return (1);
+	printf("After dequotation: \n");
+	ft_putlst_fd(lex.token_list, 1);
+	return (ft_lstclear(&lex.token_list), 0);
 }
 
 int	lex_input(t_lexer *lex)
@@ -145,34 +150,78 @@ int	handle_quotes(t_lexer *lex, char quote, int *len)
 	return (0);
 }
 
-int	check_for_quotes(t_list **token_list)
+int	check_for_dequotation(t_list **token_list)
 {
 	t_list	*current_node;
-	int		i;
 
 	current_node = *token_list;
-	if (token_list == NULL)
-		return (1);
 	while (current_node != NULL)
 	{
 		if (current_node->type == 1)
 		{
-			i = 0;
-			while (current_node->attr[i] != '\0')
+			if (ft_strchr(current_node->attr, '"') != NULL || ft_strchr(current_node->attr, '\'') != NULL)
 			{
-				if (current_node->attr[i] == '"' || current_node->attr[i] == '\'')
-				{
-					if (current_node->attr == NULL)
-						return (1);
-					ft_putendl_fd(current_node->attr, 1);
-					continue;
-				}
-				i++;
+				current_node->attr = handle_dequotation(current_node->attr, 0, 0);
+				if (current_node->attr == NULL)
+					return (1);
 			}
 		}
 		current_node = current_node->next;
 	}
 	return (0);
+}
+
+char	*handle_dequotation(char *to_trim, int i, int j)
+{
+	char	*trim_str;
+	char	quote;
+
+	trim_str = (char *)malloc((get_dequoted_strlen(to_trim) + 1) * (sizeof(char)));
+	if (trim_str == NULL)
+		return (NULL);
+	while (to_trim[i] != '\0')
+	{
+		if (to_trim[i] == '\'' || to_trim[i] == '"')
+		{
+			quote = to_trim[i++];
+			while (to_trim[i] != quote)
+				trim_str[j++] = to_trim[i++];
+			i++;
+		}
+		else
+			trim_str[j++] = to_trim[i++];
+	}
+	trim_str[j] = '\0';
+	return (free(to_trim), trim_str);
+}
+
+int	get_dequoted_strlen(char *str)
+{
+	char	quote;
+	int		num_of_quotes;
+	int		i;
+	
+	num_of_quotes = 0;
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == '\'' || str[i] == '"')
+		{
+			num_of_quotes++;
+			quote = str[i++];
+			while (str[i] != '\0')
+			{
+				if (str[i] == quote)
+				{
+					num_of_quotes++;
+					break ;
+				}
+				i++;
+			}
+		}
+		i++;
+	}
+	return (ft_strlen(str) - num_of_quotes);
 }
 
 int	check_for_expansion(t_list **token_list, char **envp)
@@ -188,17 +237,17 @@ int	check_for_expansion(t_list **token_list, char **envp)
 			i = 0;
 			while (current_node->attr[i] != '\0')
 			{
-				if (current_node->attr[i] == '\'' && current_node->in_squotes == false)
-					current_node->in_squotes = true;
-				else
-					current_node->in_squotes = false;
-				if (current_node->attr[i] == '$' && current_node->in_squotes == false)
+				if (current_node->attr[i] == '$')
 				{
 					current_node->attr = handle_expansion(current_node->attr, envp, &i);
 					if (current_node->attr == NULL)
 						return (1);
 					continue;
 				}
+				if (current_node->attr[i] == '\'' && current_node->in_squotes == false)
+					current_node->in_squotes = true;
+				else
+					current_node->in_squotes = false;
 				i++;
 			}
 		}
@@ -229,19 +278,19 @@ char	*handle_expansion(char *to_expand, char **envp, int *i)
 	// quote = 0;
 	// if (to_expand[*i] == '\'' || to_expand[*i] == '"')
 	// 	quote = 1;
-	printf("%s\n", to_expand + pos);
-	printf("To read chars: %d\n", len - 1);// - quote);
+	// printf("%s\n", to_expand + pos);
+	// printf("To read chars: %d\n", len - 1);// - quote);
 	j = -1;
 	while (envp[++j] != NULL)
 	{
 		//if (ft_strnstr(envp[j], to_expand + pos, len - 1) != NULL) //&& envp[j][len - 1] == '=')
 		if (ft_strncmp(envp[j], to_expand + pos, len - 1) == 0)
 		{
-			printf("%s\n", "Found");
+			// printf("%s\n", "Found");
 			return (*i = 0, handle_valid_expansion(to_expand, envp[j], len, pos));
 		}
 	}
-	printf("Never found\n");
+	// printf("Never found\n");
 	return (*i = 0, handle_invalid_expansion(to_expand, len));
 }
 
@@ -252,13 +301,13 @@ char	*handle_valid_expansion(char *to_expand, char *env, int len, int pos)
 	int		i;
 	int		j;
 	
-	ft_putendl_fd(env, 1);
-	ft_putendl_fd(env + len, 1);
+	// ft_putendl_fd(env, 1);
+	// ft_putendl_fd(env + len, 1);
 	exp_var = ft_substr(env, len, ft_strlen(env) - len);
 	if (exp_var == NULL)
 		return (NULL);
-	printf("LEN: %d\n", len);
-	ft_putendl_fd(exp_var, 1);
+	// printf("LEN: %d\n", len);
+	// ft_putendl_fd(exp_var, 1);
 	exp_str = (char *)malloc(((ft_strlen(to_expand) - len + ft_strlen(exp_var) + 1) * sizeof(char)));
 	if (exp_str == NULL)
 		return (free(exp_var), NULL);
@@ -273,7 +322,7 @@ char	*handle_valid_expansion(char *to_expand, char *env, int len, int pos)
 	while (to_expand[j] != '\0')
 		exp_str[i++] = to_expand[j++];
 	exp_str[i] = '\0';
-	ft_putendl_fd(exp_str, 1);
+	// ft_putendl_fd(exp_str, 1);
 	return (free(to_expand), free(exp_var), exp_str);
 }
 
@@ -293,4 +342,30 @@ char	*handle_invalid_expansion(char *str, int len)
 	ft_strlcat(new_str + i, str + i + len, ft_strlen(str) - len - i + 1);
 	free(str);
 	return (new_str);	
+}
+
+size_t	ft_strlen_except(const char *str, char *exceptions)
+{
+	size_t	len;
+	bool	is_except;
+	int		i;
+	int		j;
+
+	len = 0;
+	i = 0;
+	while (str[i] != '\0')
+	{
+		is_except = false;
+		j = 0;
+		while (exceptions[j] != '\0')
+		{
+			if (str[i] == exceptions[j])
+				is_except = true;
+			j++;
+		}
+		if (is_except == false)
+			len++;
+		i++;
+	}
+	return (len);
 }
