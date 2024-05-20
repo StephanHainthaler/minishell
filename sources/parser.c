b20619bd35ddb254c52181cee58f5e6b682e7784
Jayu <jayuze@gmail.com>
2024-05-20 16:14:14 +0200

    trying new parser that return t_cmd struct for every command

--- sources/parser.c
+++ sources/parser.c
@@ -6,13 +6,41 @@
 /*   By: juitz <juitz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 15:28:56 by juitz             #+#    #+#             */
-/*   Updated: 2024/05/20 15:30:04 by juitz            ###   ########.fr       */
+/*   Updated: 2024/05/20 16:11:39 by juitz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../headers/minishell.h"
 
-t_cmd	*get_cmd(t_minishell *ms)
+int	count_pipes(t_minishell *ms)
 {
-	
+	t_list *current;
+
+	current = ms->lex->token_list;
+	while (current != NULL)
+	{
+		if (ms->lex->token_list->type == PIPE)
+			ms->cmd->num_of_simp_cmds++;
+		current = current->next;
+	}
+	ms->lex->token_list = current;
+	return (ms->cmd->num_of_simp_cmds + 1);
+}
+
+
+t_cmd	*new_cmd(t_minishell *ms)
+{
+	t_cmd	*new_cmd;
+	t_list	*current;
+	int i;
+
+	i = 0;
+	current = ms->lex->token_list;
+	while (current)
+	{
+		while (i < ms->cmd->num_of_simp_cmds + 1)
+		{
+		
+		}
+	}
 }
