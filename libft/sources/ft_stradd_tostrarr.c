6f9522f976e3d34f32a11111d3f78bc2dcc0ab63
Stephan Hainthaler <shaintha@c2r4p4.42vienna.com>
2024-05-27 13:49:06 +0200

    Leaks are gone. Execution can be tested now.

--- libft/sources/ft_stradd_tostrarr.c
+++ libft/sources/ft_stradd_tostrarr.c
@@ -6,7 +6,7 @@
 /*   By: shaintha <shaintha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 10:53:43 by shaintha          #+#    #+#             */
-/*   Updated: 2024/05/27 12:13:31 by shaintha         ###   ########.fr       */
+/*   Updated: 2024/05/27 12:36:36 by shaintha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,23 +33,23 @@ char	**ft_stradd_tostrarr(char **strarr, char *str)
 	new_strarr[i++] = ft_strdup(str);
 	if (new_strarr == NULL)
 		return (ft_free_strarr(new_strarr), NULL);
-	//new_strarr[i] = NULL;
+	new_strarr[i] = NULL;
 	return (ft_free_strarr(strarr), new_strarr);
 }
 
-int	main(void)
-{
-	char	**split;
-	char	*str;
+// int	main(void)
+// {
+// 	char	**split;
+// 	char	*str;
 
-	split = ft_split("Hello World TEst", ' ');
-	//split = NULL;
-	str = "HELLO_WORLD";
-	ft_putstrarr_fd(split, 1);
-	ft_putendl_fd(str, 1);
-	split = ft_stradd_tostrarr(split, str);
-	ft_putendl_fd("test4", 1);
-	ft_putstrarr_fd(split, 1);
-	ft_putendl_fd("test5", 1);
-	ft_free_strarr(split);
-}
+// 	split = ft_split("Hello1 World1", ' ');
+// 	//split = NULL;
+// 	str = "HELLO2_WORLD2";
+// 	ft_putstrarr_fd(split, 1);
+// 	ft_putendl_fd(str, 1);
+// 	split = ft_stradd_tostrarr(split, str);
+// 	ft_putendl_fd("test1", 1);
+// 	ft_putstrarr_fd(split, 1);
+// 	ft_putendl_fd("test2", 1);
+// 	ft_free_strarr(split);
+// }
--- sources/parser.c
+++ sources/parser.c
@@ -6,7 +6,7 @@
 /*   By: shaintha <shaintha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 15:28:56 by juitz             #+#    #+#             */
-/*   Updated: 2024/05/27 11:56:53 by shaintha         ###   ########.fr       */
+/*   Updated: 2024/05/27 13:48:29 by shaintha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,63 +28,47 @@ int	parse_input(t_minishell *ms)
 	return (0);
 }
 
-// int	parse_input(t_minishell *ms)
-// {
-// 	if (initialize_executor(ms) == 1)
-// 		return (1);
-// 	// if (get_cmds(ms->exec, &ms->lex->token_list) == 1)
-// 	// 	return (free_executor(ms->exec), 1);
-// 	return (0);
-// }
-
-
 int		get_cmds(t_executor *exec, t_list **list)
 {
 	t_list	*current;
 	int i;
-	int	j;
 
 	i = 0;
-	j = 0;
 	current = *list;
-	while (current) //&& i < ms->cmds->num_of_simp_cmds + 1)
+	while (current)
 	{
 		if (current->type == WORD)
 		{
 			exec->cmds[i]->simp_cmd = ft_stradd_tostrarr(exec->cmds[i]->simp_cmd, current->attr);
 			if (exec->cmds[i]->simp_cmd == NULL)
-				return (1); //free)
-			printf("Added %s to simp_cmd\n", exec->cmds[i]->simp_cmd[j]);
-			j++;
+				return (1);
 		}
-		if (current->type == RE_IN || current->type == APPEND)
+		if (current->type == RE_IN)
 		{
 			current = current->next;
-			exec->cmds[i]->infile = current->attr;
+			if (exec->cmds[i]->infile != NULL)
+				free(exec->cmds[i]->infile);
+			exec->cmds[i]->infile = ft_strdup(current->attr);
 			exec->cmds[i]->in_fd = get_fd(exec->cmds[i]->infile, true);
 		}
 		if (current->type == RE_OUT)
 		{
 			current = current->next;
-			exec->cmds[i]->outfile = current->attr;
+			if (exec->cmds[i]->outfile != NULL)
+				free(exec->cmds[i]->outfile);
+			exec->cmds[i]->outfile = ft_strdup(current->attr);
 			exec->cmds[i]->out_fd = get_fd(exec->cmds[i]->outfile, false);
 		}
 		if (current->type == PIPE)
 		{
-			printf("End of cmd\n");
-			//current = current->next;
-			//ft_print_cmd(exec->cmds[i]);
-			//current = current->next;
 			exec->cmds[i]->cmd_nbr = i;
-			j = 0;
 			i++;
 		}
-			//else if (current->type == HERE_DOC)
+		//of (APPEND)
+		//if (current->type == HERE_DOC)
 			//NULL CHECK
-		printf("Next node\n");
 		current = current->next;
 	}
-	printf("End of cmd\n");
 	return (0);
 }
 
