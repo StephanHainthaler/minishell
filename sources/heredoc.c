ee98fd891649fc8c4539f608a56029cbc390bffc
Jayu <jayuze@gmail.com>
2024-05-28 17:19:01 +0200

    unfinished scuffed heredoc implementation

--- sources/heredoc.c
+++ sources/heredoc.c
@@ -6,13 +6,13 @@
 /*   By: juitz <juitz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 11:02:54 by juitz             #+#    #+#             */
-/*   Updated: 2024/05/28 14:45:35 by juitz            ###   ########.fr       */
+/*   Updated: 2024/05/28 17:12:42 by juitz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../headers/minishell.h"
 
-void	parse_heredoc(t_minishell *ms)
+char	**parse_heredoc(t_minishell *ms)
 {
 	char *line;
 	char **full_input;
@@ -25,9 +25,12 @@ void	parse_heredoc(t_minishell *ms)
 		current = current->next;
 		delim = current->attr;
 	}
-	while (ft_strncmp(ms->lex->input, delim, ft_strlen(delim)) == 0)
+	while (1)
 	{
 		line = readline("heredoc>");
 		full_input = ft_split(line, '\n');
+		if (ft_strncmp(ms->lex->input, delim, ft_strlen(delim)) == 0)
+			return(free(line), free(delim), full_input);
 	}
+	return (NULL);
 }
