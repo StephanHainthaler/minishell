d01417e858a1189e66aa909561f4ce17194a6d35
Jayu <jayuze@gmail.com>
2024-06-12 16:53:48 +0200

    idk

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: juitz <juitz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 09:06:32 by shaintha          #+#    #+#             */
-/*   Updated: 2024/06/11 15:26:51 by juitz            ###   ########.fr       */
+/*   Updated: 2024/06/12 16:49:59 by juitz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,9 +27,8 @@ int	main(int argc, char *argv[], char *env[])
 		if (parse_input(&ms) == 1)
 			return (free_lexer(ms.lex), free_executor(ms.exec), ft_free_strarr(ms.envp), rl_clear_history(), 1);
 		free_lexer(ms.lex);
-		if (handle_builtin(ms.exec->cmds->simp_cmd, ms.envp) == 0)
+		if (handle_builtin(ms.exec->cmds->simp_cmd, ms.exec->envp) == 0)
 			return (free_executor(ms.exec), ft_free_strarr(ms.envp), rl_clear_history(), 1);
-			
 		if (execute_input(&ms) == 1)
 			return (free_executor(ms.exec), free(ms.envp), 1);
 		free_executor(ms.exec);
