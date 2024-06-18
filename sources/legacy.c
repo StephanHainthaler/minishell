e4cc3bbdfeb0cc3cccdbc6599ef0caa1db1d5510
Stephan Hainthaler <144452308+StephanHainthaler@users.noreply.github.com>
2024-06-18 13:14:39 +0000

    Added a fail safe for token parsing.

--- sources/legacy.c
+++ sources/legacy.c
@@ -12,6 +12,35 @@
 
 #include "../headers/minishell.h"
 
+int	main(int argc, char *argv[], char *env[])
+{
+	t_minishell	ms;
+	
+	if (argc != 1)
+		return (1);
+	if (initialize_minishell(&ms, argc, argv, env) == 1)
+		return (1);
+	while (true)
+	{
+		error_check = read_input(&ms);
+		if (error_check == 1)
+			return (free_lexer(ms.lex), ft_free_strarr(ms.envp), rl_clear_history(), 1);
+		if (error_check == 2)
+		{
+			free_lexer(ms.lex);
+			continue ;
+		}
+		if (parse_input(&ms) == 1)
+			return (free_lexer(ms.lex), free_executor(ms.exec), ft_free_strarr(ms.envp), rl_clear_history(), 1);
+		//same check as above but we have no lines :c
+		free_lexer(ms.lex);
+		if (execute_input(&ms) == 1)
+			return (free_executor(ms.exec), ft_free_strarr(ms.envp), 1);
+		free_executor(ms.exec);
+	}
+	return (0);
+}
+
 int	multiple_execution(t_executor *exec)
 {
 	int	i;
